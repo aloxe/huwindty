bb7496060881e3ad39ec3871182690804448587f
aloxe <github@blog.re>
2024-12-20 15:05:47 +0100

    make a shorter list but with images

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,16 +3,8 @@ module.exports = {
     collect: {
       "url": [
         "http://localhost/index.html",
-        "http://localhost/documentation/",
         "http://localhost/documentation/images/",
-        "http://localhost/documentation/styles/",
-        "http://localhost/documentation/cms/",
         "http://localhost/examples/",
-        "http://localhost/examples/images/",
-        "http://localhost/examples/images/local/wasps/",
-        "http://localhost/examples/images/local/sheep/",
-        "http://localhost/examples/images/global/cows/",
-        "http://localhost/examples/images/global/heron/",
       ],
       staticDistDir: './_site',
       staticDirFileDiscoveryDepth: 1,
